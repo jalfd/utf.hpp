5e7ac1b84b510c3817222cd33429e8241bad56dc
jalf <git@jalf.dk>
2013-03-29 00:41:03 +0100

    add license stuff

--- tests.cpp
+++ tests.cpp
@@ -1,3 +1,9 @@
+
+//          Copyright Jesper Dam 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #define CATCH_CONFIG_MAIN
 #include <catch/catch.hpp>
 
